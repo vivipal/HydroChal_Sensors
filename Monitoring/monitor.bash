3e06fa49005c3db884d7e5994fc2566a2bd837cc
V. Pelle <virgile.pelle@ensta-bretagne.org>
2022-04-06 11:50:26 +0200

    add config for rx

--- Monitoring/monitor.bash
+++ Monitoring/monitor.bash
@@ -31,8 +31,8 @@ do
 
 pgrep -x IMU_exe > /dev/null  && echo -e "IMU_exe found" || (echo -e "IMU_exe not found" && ../executable/IMU_exe ${arr[0]} ${arr[1]} >/dev/null&)
 pgrep -x logger_exe  > /dev/null && echo -e  "logger_exe found" || (echo -e "logger_exe not found" && ../executable/logger_exe ${arr[2]} ${arr[3]} >/dev/null&)
-pgrep -x rx_exe  > /dev/null && echo -e  "rx_exe found" || (echo -e "rx_exe not found" && ../executable/rx_exe ${arr[4]} > /dev/null&)
-pgrep -x WS_exe > /dev/null  && echo -e  "WS_exe found" || (echo -e "WS_exe not found" && ../executable/WS_exe ${arr[5]} ${arr[6]} >/dev/null&)
+pgrep -x rx_exe  > /dev/null && echo -e  "rx_exe found" || (echo -e "rx_exe not found" && ../executable/rx_exe ${arr[4]} ${arr[5]}> /dev/null&)
+pgrep -x WS_exe > /dev/null  && echo -e  "WS_exe found" || (echo -e "WS_exe not found" && ../executable/WS_exe ${arr[6]} ${arr[7]} >/dev/null&)
 
 
 echo -e "\n"
